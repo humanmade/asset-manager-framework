6061144f911d779407289abce1aab3a7ad1303e5
John Blackbourn <johnbillion@gmail.com>
2020-03-08 00:45:28 +0000

    Add the missing functions file.

---
+++ src/functions.js
@@ -0,0 +1,41 @@
+
+function get_click_handler( item ) {
+
+	let click_handler = item.click;
+	const attribute = ( item.requires.library ? 'library' : 'selection' );
+
+	return function() {
+		const selection_state = this.controller.state().get( attribute );
+		const new_attachments = selection_state.models.filter(model => ! model.attributes.attachmentExists);
+
+		click_handler = _.bind( click_handler, this );
+
+		if ( ! new_attachments.length ) {
+			click_handler();
+			return;
+		}
+
+		let request = wp.ajax.post(
+			'amf-select',
+			{
+				'selection' : selection_state.toJSON(),
+				'post' : wp.media.view.settings.post.id,
+			}
+		);
+
+		request.done( response => {
+			Object.keys(response).forEach( key => {
+				selection_state.get( key ).set( 'id', response[ key ] );
+			});
+
+			click_handler();
+		} ).fail( response => {
+			console.log('=== failed ===');
+			console.log(response);
+			// @TODO call click_handler
+		} );
+
+	}
+}
+
+export { get_click_handler }
