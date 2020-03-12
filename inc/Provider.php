2721f30b7c50f144a8134deaae8f4e3523dd4e03
John Blackbourn <johnbillion@gmail.com>
2020-03-12 13:46:39 +0000

    More prep for handling the post_parent parameter.

--- inc/Provider.php
+++ inc/Provider.php
@@ -49,12 +49,13 @@ abstract class Provider {
 	}
 
 	final public function request_items( array $args ) : MediaList {
-		if ( ! empty( $args['post_parent'] ) ) {
+		if ( isset( $args['post_parent'] ) ) {
 			// @TODO
 			// special case, only return attachments that exist.
 			// run a WP_Query for attachments with post parent and
 			// set the attachment status on all of the items
 			// and return
+			// note that post_parent can be 0
 		}
 
 		if ( isset( $args['post_mime_type'] ) ) {
