337d0278c789b3b91993093ca68c09354fe23932
Thorsten Frommen <info@tfrommen.de>
2021-02-18 10:21:51 +0100

    Add missing audio metadata

--- inc/Audio.php
+++ inc/Audio.php
@@ -10,4 +10,28 @@ declare( strict_types=1 );
 namespace AssetManagerFramework;
 
 class Audio extends Playable {
+
+	public $album = '';
+	public $artist = '';
+
+	final public function set_album( string $album ) : self {
+		$this->album = $album;
+		$this->meta['album'] = $album;
+
+		return $this;
+	}
+
+	final public function set_artist( string $artist ) : self {
+		$this->artist = $artist;
+		$this->meta['artist'] = $artist;
+
+		return $this;
+	}
+
+	final public function set_bitrate( int $bitrate, string $bitrate_mode = '' ) : self {
+		$this->meta['bitrate'] = $bitrate;
+		$this->meta['bitrate_mode'] = $bitrate_mode;
+
+		return $this;
+	}
 }
