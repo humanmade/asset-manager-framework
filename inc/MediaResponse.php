a845dfa317041d358671c59c34be477f9ec122ac
Robert O'Rourke <robert@o-rourke.org>
2021-09-22 14:18:25 +0100

    Throw exception with bad per page value

--- inc/MediaResponse.php
+++ inc/MediaResponse.php
@@ -9,6 +9,8 @@ declare( strict_types=1 );
 
 namespace AssetManagerFramework;
 
+use RangeException;
+
 class MediaResponse {
 
 	/**
@@ -39,8 +41,20 @@ class MediaResponse {
 	 * @param int $total The total number of results available.
 	 * @param int $per_page The number of items requested per page, defaults to 40 in media
 	 *                      modal requests.
+	 * @throws RangeException If $per_page value is less than or equal to 0.
 	 */
 	public function __construct( ?MediaList $media_list = null, int $total = 0, int $per_page = 40 ) {
+		// Fail early if provided per page value is not valid.
+		if ( $per_page <= 0 ) {
+			throw new RangeException(
+				sprintf(
+					/* translators: %d: Items per page value in error message */
+					__( 'Media items per page value must be greater than zero, %d given.', 'asset-manager-framework' ),
+					$per_page
+				)
+			);
+		}
+
 		$this->media_list = $media_list ?? new MediaList();
 		$this->total = $total;
 		$this->per_page = $per_page;
