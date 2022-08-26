37ee1ee2ede923fb9325d6754090f33fcf02809c
Robin Devitt <robin@humanmade.com>
2022-08-26 13:29:04 +0200

    Restore changes

--- inc/namespace.php
+++ inc/namespace.php
@@ -214,7 +214,7 @@ function ajax_select() : void {
 function replace_attached_file( $file, int $attachment_id ) : string {
 	$attachment = get_post( $attachment_id );
 	if ( ! is_amf_asset( $attachment ) ) {
-		return ( $file ? $file : '' );
+		return $file;
 	}
 
 	$metadata = wp_get_attachment_metadata( $attachment_id, true );
